fc279b4203d9361ed43734da9fc911b9726ecbbe
Phablo <miranda.phablo@live.com>
2024-10-14 13:05:25 -0300

    comentarios

--- index.js
+++ index.js
@@ -15,21 +15,19 @@ function determinarClasse(xp) {
         return "Imortal";
     } else if (xp >= 10001) {
         return "Radiante";
-    } else {
-        return "Desconhecido";
     }
 }
 
-// Função para gerar um número aleatório entre min e max (inclusive)
+// Número aleatório de XP
 function gerarNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Variáveis para armazenar o nome e a quantidade de XP do herói
+// Nome e a quantidade de XP do herói
 const nomeHeroi = "Sr do Passinho"; // Substitua pelo nome do herói
-const xpHeroi = gerarNumeroAleatorio(0, 11000); // Gera um número aleatório de XP entre 0 e 11000
+const xpHeroi = gerarNumeroAleatorio(0, 11000);
 
-// Determina a classe do herói com base na quantidade de XP
+// Classe do herói com base na quantidade de XP
 const nivelHeroi = determinarClasse(xpHeroi);
 
 // Exibe a mensagem final
