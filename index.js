6fc6016acd5a23a9c280dea2a3ab3e5aece76d27
Phablo <miranda.phablo@live.com>
2024-10-14 12:04:26 -0300

    texto criado

---
+++ index.js
@@ -0,0 +1,23 @@
+let xpDoMob = 1003
+let heroi = "Sr do Passinho"
+let xpHeroi = 4050
+
+
+
+
+let texto =`O Herói` + " " + (heroi) + `, está em uma jornada para se tornar o melhor guerreiro do reino de Binhocicaba.
+
+            Então ele entrou em uma Dungeon para ver o seu nível de XP.
+
+            Logo deu de cara com Goblin Bombardeiro de nível` + " " + (xpDoMob) + `
+
+            Entrou em batalha e o loot caiu depois de golpes!` //colocar números de golpes 
+
+
+
+
+console.log(texto);
+
+
+
+
