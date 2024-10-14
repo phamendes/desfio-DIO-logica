1609e9d54b2e42535b895bc05eae4336009c4b46
Phablo <miranda.phablo@live.com>
2024-10-14 13:02:51 -0300

    colocado um gerador de xp aleatorio

--- index.js
+++ index.js
@@ -20,12 +20,17 @@ function determinarClasse(xp) {
     }
 }
 
+// Função para gerar um número aleatório entre min e max (inclusive)
+function gerarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // Variáveis para armazenar o nome e a quantidade de XP do herói
-const nomeHeroi = "Arthur"; // Substitua pelo nome do herói
-const xpHeroi = 8500; // Substitua pela quantidade de XP do herói
+const nomeHeroi = "Sr do Passinho"; // Substitua pelo nome do herói
+const xpHeroi = gerarNumeroAleatorio(0, 11000); // Gera um número aleatório de XP entre 0 e 11000
 
 // Determina a classe do herói com base na quantidade de XP
 const nivelHeroi = determinarClasse(xpHeroi);
 
 // Exibe a mensagem final
-console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`);
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi} com ${xpHeroi} XP`);
