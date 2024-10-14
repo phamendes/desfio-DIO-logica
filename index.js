b8cd096376a0832b69ecd4b0de04ee46e51298fd
Phablo <miranda.phablo@live.com>
2024-10-14 12:45:44 -0300

    troca  da estrutura do código

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-let xpDoMob = 1003
-let heroi = "Sr do Passinho"
-let xpHeroi = 4050
+function determinarClasse(xp) {
+    if (xp < 1000) {
+        return "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        return "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        return "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        return "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        return "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        return "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        return "Imortal";
+    } else if (xp >= 10001) {
+        return "Radiante";
+    } else {
+        return "Desconhecido";
+    }
+}
 
+// Variáveis para armazenar o nome e a quantidade de XP do herói
+const nomeHeroi = "Arthur"; // Substitua pelo nome do herói
+const xpHeroi = 8500; // Substitua pela quantidade de XP do herói
 
+// Determina a classe do herói com base na quantidade de XP
+const nivelHeroi = determinarClasse(xpHeroi);
 
-
-let texto =`O Herói` + " " + (heroi) + `, está em uma jornada para se tornar o melhor guerreiro do reino de Binhocicaba.
-
-            Então ele entrou em uma Dungeon para ver o seu nível de XP.
-
-            Logo deu de cara com Goblin Bombardeiro de nível` + " " + (xpDoMob) + `
-
-            Entrou em batalha e o loot caiu depois de golpes!` //colocar números de golpes 
-
-
-
-
-console.log(texto);
-
-
-
-
+// Exibe a mensagem final
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`);
